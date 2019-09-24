dbb1aeb5adddc58e8150831d627b0c02ada9f606
Junaid Qureshi <jaq1973@gmail.com>
2019-09-24 14:47:50 -0400

    Update EmailService for call ED service

--- src/main/java/com/ilinksolutions/UKVisaEmail/bservices/UKVisaService.java
+++ src/main/java/com/ilinksolutions/UKVisaEmail/bservices/UKVisaService.java
@@ -1,5 +1,10 @@
 package com.ilinksolutions.UKVisaEmail.bservices;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import com.ilinksolutions.UKVisaEmail.data.UKVisaDAO;
@@ -37,7 +42,7 @@ public class UKVisaService
 								"\"lastName\": \"" + message.getLastName() + "\"," +
 								"\"contactNo\": \"" + message.getContactNo() + "\"," +
 								"\"email\": \"" + message.getEmail() + "\"}";
-		String encryptedString = AES256Manager.encryptMessage(messageString);
+		String encryptedString = encryptMessage(messageString); //AES256Manager.encryptMessage(messageString);
 		EmailManager eMail = new EmailManager(subject, text);
 		eMail.send(encryptedString);
 		return returnValue;
@@ -54,9 +59,40 @@ public class UKVisaService
 				"\"lastName\": \"" + message.getLastName() + "\"," +
 				"\"contactNo\": \"" + message.getContactNo() + "\"," +
 				"\"email\": \"" + message.getEmail() + "\"}";
-		String encryptedString = AES256Manager.encryptMessage(messageString);
+		String encryptedString = encryptMessage(messageString); AES256Manager.encryptMessage(messageString);
 		EmailManager eMail = new EmailManager(subject, text);
 		eMail.send(encryptedString);
 		return dao.updateEntry(id, message);
 	}
+	
+	
+	public String encryptMessage(String message)
+    {
+        String output = "";
+		try
+        {
+            URL url = new URL("http://ilinkp4-ilinkp4.b9ad.pro-us-east-1.openshiftapps.com/UKVisaED/encryptmsg");
+            HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+            httpConnection.setDoOutput(true);
+            httpConnection.setRequestMethod("POST");
+            httpConnection.setRequestProperty("Content-Type", "application/json");
+            OutputStream oStream = httpConnection.getOutputStream();
+            oStream.write(message.getBytes());
+            
+            if (httpConnection.getResponseCode() != 200)
+            {
+                throw new  RuntimeException("Encryption/Dcryption Services Error: Error Code ");
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader((httpConnection.getInputStream())));
+    
+            while ((output = br.readLine()) != null)
+            {
+            }
+            oStream.flush();
+            httpConnection.disconnect();
+        } catch (Exception e) {
+        	  throw new  RuntimeException("Encryption/Dcryption Services Error: Error Code ", e);
+        }
+		return output;
+    }
 }
