50165993aa8cfbd7e329bf4e9911b8283b095212
Junaid Qureshi <jaq1973@gmail.com>
2019-09-25 14:20:45 -0400

    Fix Swagger Config

--- src/main/java/com/ilinksolutions/UKVisaEmail/config/SwaggerConfig.java
+++ src/main/java/com/ilinksolutions/UKVisaEmail/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.ilinksolutions.UKVisaEmail.config;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,7 +11,10 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
+@Configuration
 public class SwaggerConfig {
 	@Bean
     public Docket productApi() {
